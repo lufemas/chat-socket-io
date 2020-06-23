85e69683c6d122306ecdee7c1be35f50f3c8d473
lufemas <maschietto.jr@gmail.com>
2020-06-22 23:51:35 -0400

    port fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express  = require('express')
 const app      = express()
-const PORT     = 8080
+const PORT     = process.env.PORT || 8080
 const server   = app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}`)})
 const io       = require('socket.io').listen(server)
 
